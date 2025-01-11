0f70a6476a89ed61eadb56a4f10feb30675e0f90
Bloodlinebots <githubsbots@gmail.com>
2025-01-12 00:36:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ ADS_MODE = getenv("ADS_MODE", None)
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6726372149))
+OWNER_ID = int(getenv("OWNER_ID", 7692605600))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Patel070707/SITARAXMUSICAMIT",
+    "https://github.com/Bloodlinebots/SITARAXMUSICsmallrepo.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
