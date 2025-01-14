674fffc6a005500dab3bdf8c49a8b26c36d68b4e
Bloodlinebots <githubsbots@gmail.com>
2025-01-14 16:41:04 +0530

    Update play.py

--- MoonXMusic/utils/inline/play.py
+++ MoonXMusic/utils/inline/play.py
@@ -60,7 +60,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ], 
-            )
+            
          [
             InlineKeyboardButton(
                 text="• ᴜᴘᴅᴀᴛᴇ •", url ="https://t.me/ll_BOTCHAMBER_ll",
